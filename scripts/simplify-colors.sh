8ae1d3608b5cabdec9285e8dcc9b759af76c0a18
Ricardo Jesus <rj.bcjesus@gmail.com>
2018-09-05 19:08:56 +0100

    Fix globing of files for `make print'

--- scripts/simplify-colors.sh
+++ scripts/simplify-colors.sh
@@ -9,16 +9,17 @@
 # However, the final document will have less color pages. In some cases
 # this will reduce the cost of printing the document.
 #
-# REQUIREMENTS: pdftops, gs-noX11 (from ghostscript)
+# REQUIREMENTS: gs, pdftops
+
+set -e
 
 BUILD="build/tmp"
 
 function forceGray {
     pdftops -level3sep "$1" "$1-aux.ps"
-    gs -o "$1" -sDEVICE=pdfwrite -sColorConversionStrategy=Gray -sProcessColorModel=DeviceGray "$1-aux.ps"
+    gs -q -o "$1" -sDEVICE=pdfwrite -sColorConversionStrategy=Gray -sProcessColorModel=DeviceGray "$1-aux.ps"
     rm "$1-aux.ps"
 }
-
 
 if [ $# -eq 0 ]; then
     echo "Usage: $0 filename.pdf"
@@ -29,26 +30,19 @@ fi
 FILENAME="$1"
 
 if [ ! -f ${FILENAME} ]; then
-    echo "${FILENAME} not found!"
+    >&2 echo "${FILENAME} not found!"
     exit 1
 fi
 
-if [ -d "$BUILD" ]; then
-    rm -f "$BUILD"/*
-else
-    mkdir -p "$BUILD"
-fi
+mkdir -p "$BUILD"
+rm -f "$BUILD"/*
 
-if [ ! -d "$BUILD" ] || [ ! -w "$BUILD" ]; then
-    echo "Unable to write to $BUILD"
-    exit 1
-fi
+gs -q -sDEVICE=pdfwrite -dSAFER -o "$BUILD/%d-`basename $FILENAME .pdf`-split.pdf" "$FILENAME"
 
-gs -sDEVICE=pdfwrite -dSAFER -o "$BUILD/%d-`basename $FILENAME .pdf`-split.pdf" "$FILENAME" || exit 1
-
-for i in "$BUILD"/*-"`basename ${FILENAME} .pdf`-split.pdf"; do
+for i in `ls -v "$BUILD"/*-split.pdf`; do
     echo "$i"
-    COLORS=$(convert "$i" -colorspace CMYK -unique-colors txt:- | grep  \%| grep -v "0%,0%,0%" | cut -d ' ' -f 2 | sed -e "s/%//g" -e "s/(//g" -e "s/)//g")
+
+    COLORS=$(convert "$i" -colorspace CMYK -unique-colors txt:- | grep \% | grep -v "0%,0%,0%" | cut -d ' ' -f 2 | sed -e "s/%//g" -e "s/(//g" -e "s/)//g")
     if [ "X${COLORS}" == "X" ]; then
         forceGray "$i"
         continue
@@ -56,7 +50,7 @@ for i in "$BUILD"/*-"`basename ${FILENAME} .pdf`-split.pdf"; do
 
     GRAY=1
     for c in "$COLORS"; do
-        COLOR=$(echo "$c" |sed -e "s/,/ /g")
+        COLOR=$(echo "$c" | sed -e "s/,/ /g")
         COLOR_C=$(echo -n "$COLOR" | cut -d ' ' -f 1)
         COLOR_M=$(echo -n "$COLOR" | cut -d ' ' -f 2)
         COLOR_Y=$(echo -n "$COLOR" | cut -d ' ' -f 3)
@@ -71,6 +65,6 @@ for i in "$BUILD"/*-"`basename ${FILENAME} .pdf`-split.pdf"; do
     fi
 done
 
-gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile="${FILENAME%.*}-final.pdf" $(ls "$BUILD"/*-split.pdf | sort -n -t \/ -k 2)
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile="${FILENAME%.*}-final.pdf" $(ls -v "$BUILD"/*-split.pdf)
 
 rm -rf "$BUILD"
