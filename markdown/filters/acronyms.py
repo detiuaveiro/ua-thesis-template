ce02af64f351aa5a548c785ece678e0ea8a0e880
Joao-Felisberto <joaofelisberto@ua.pt>
2023-12-28 08:57:48 +0000

    Fixed acronyms not being replaced when they end in s or punctuation

--- markdown/filters/acronyms.py
+++ markdown/filters/acronyms.py
@@ -1,7 +1,7 @@
 import pandocfilters as pf
 import yaml
 
-# Acronyms dictionary
+# Acronyms set 
 acronyms = {}
 
 acros = None
@@ -12,7 +12,15 @@ def replace_acronyms(key, value, format, meta):
     if key == 'Str':
         # Check if the text is an acronym
         acronym = value
-        if (a := acronym.upper()) in acronyms:
+        a = acronym.lower()
+        c = ''
+        if not a[-1].isalnum():
+            c = a[-1].lower()
+            a = a[:-1]
+        if a.endswith('s'): # separate if 's' because a plural acronym can end with punctuation
+            c = a[-1].lower() + c
+            a = a[:-1]
+        if a in acronyms:
             cnt = acrocnt.get(a)
             if cnt is None:
                 acrocnt[a] = 1
@@ -21,13 +29,13 @@ def replace_acronyms(key, value, format, meta):
                 acrocnt[a] = 2
                 return None
             else:
-                return [pf.RawInline('latex', '\\ac{' + acronym.lower() + '}')]
+                return [pf.RawInline('latex', '\\ac{' + a + '}' + c)]
 
 if __name__ == "__main__":
     with open("metadata.yaml", "r") as f:
         try:
             acros = yaml.safe_load(f)['acronyms']
-            acronyms = {e['short']: e['long'] for e in acros}
+            acronyms = {e['id'] for e in acros}
         except yaml.YAMLError as exc:
             pass
     pf.toJSONFilter(replace_acronyms)
